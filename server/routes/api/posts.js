7319d0b017e64ce8fa3392753f29fe7e32777cef
MIKE <mike.santoro42@gmail.com>
2019-01-02 00:32:28 -0500

    get, post, delete working (backend express api)

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -10,9 +10,22 @@ router.get('/', async (req, res) => {
 });
 
 //Add Posts
+router.post('/', async (req, res) => {
+    const posts = await loadPostCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
+
 
 //Delete Posts
-
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
 
 async function loadPostCollection() {
     const client = await mongodb.MongoClient.connect('mongodb://mike42:mikeroot42@ds161179.mlab.com:61179/vue_express', {
